14b352a096d1091d092f79bdd32839f3a2156f7f
Heather McCracken <hmccracken@esri.com>
2018-08-10 09:39:09 -0400

    Update with the latest
    update with Changes

--- Lean Coffee for ArcGIS/ViewController.swift
+++ Lean Coffee for ArcGIS/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import UserNotifications
+
 
 class ViewController: UIViewController {
     @IBOutlet var startNewTopicLabel: UILabel!
@@ -23,7 +25,7 @@ class ViewController: UIViewController {
     let finalRoundTimer = 60
     var currentRoundTimer = 300
     var seconds = 300
-    
+     
     var timer = Timer()
     var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
     
@@ -41,17 +43,20 @@ class ViewController: UIViewController {
             self.resumeTapped = false
             self.pauseButton.setTitle("Pause", for: .normal)
         }
-        pauseButton.isEnabled = false
-        startButton.isEnabled = true
-        resetButton.isEnabled = false
+        
+        // Set the Button start state for new topic
+        resetButtonStartStates()
         
     }
     
     @IBAction func startTimerTapped(_ sender: UIButton)  {
         if isTimerRunning == false {
             runTimer()
-            self.startButton.isEnabled = false
-            self.resetButton.isEnabled = true
+            disableButton(theButton: startButton)
+            enableButton(theButton: resetButton)
+            
+
+
         }
     }
     
@@ -60,10 +65,12 @@ class ViewController: UIViewController {
             timer.invalidate()
             self.resumeTapped = true
             self.pauseButton.setTitle("Resume", for: .normal)
+            self.pauseButton.setTitleColor(UIColor.green, for: .normal)
         } else {
             runTimer()
             self.resumeTapped = false
             self.pauseButton.setTitle("Pause", for: .normal)
+            self.pauseButton.setTitleColor(UIColor.orange, for: .normal)
         }
     }
     
@@ -77,11 +84,12 @@ class ViewController: UIViewController {
         if self.resumeTapped == true {
             self.resumeTapped = false
             self.pauseButton.setTitle("Pause", for: .normal)
+            self.pauseButton.setTitleColor(UIColor.orange, for: .normal)
         }
         
-        pauseButton.isEnabled = false
-        startButton.isEnabled = true
-        resetButton.isEnabled = false
+        // Set the Button start state for new topic
+        resetButtonStartStates()
+
         
     }
     
@@ -100,8 +108,11 @@ class ViewController: UIViewController {
         timer.invalidate()
         myTimerLabel.text = timeString(time: TimeInterval(seconds))
         isTimerRunning = false
-        pauseButton.isEnabled = false
-        startButton.isEnabled = true
+
+        
+        // Set the Button start state for new topic
+        resetButtonStartStates()
+        
     }
     
     @IBAction func thumbsDownTapped(_ sender: UIButton)  {
@@ -110,14 +121,86 @@ class ViewController: UIViewController {
         timer.invalidate()
         myTimerLabel.text = timeString(time: TimeInterval(seconds))
         isTimerRunning = false
-        pauseButton.isEnabled = false
-        startButton.isEnabled = true
+
+        // Set the Button start state for new topic
+        resetButtonStartStates()
+        
+    }
+    
+    func resetButtonStartStates() {
+
+        enableButton(theButton: startButton)
+        disableButton(theButton: pauseButton)
+        disableButton(theButton: resetButton)
+    }
+    
+    func changeButtonState(theButton: UIButton, setEnabled: Bool, titleColor: UIColor) {
+        theButton.isEnabled = setEnabled
+        theButton.setTitleColor(titleColor, for: .normal)
+        
+    }
+    
+    func enableButton(theButton: UIButton) {
+        let titleUIColor: UIColor
+        
+        switch (theButton){
+            
+        case startButton:
+            titleUIColor = UIColor.green
+        case pauseButton:
+            titleUIColor = UIColor.orange
+        case resetButton:
+            titleUIColor = UIColor.white
+            
+        default:
+            titleUIColor = UIColor.lightGray
+        }
+        
+        changeButtonState(theButton: theButton, setEnabled: true, titleColor: titleUIColor)
+    }
+    
+    func disableButton(theButton: UIButton) {
+        theButton.isEnabled = false
+        theButton.setTitleColor(UIColor.lightGray, for: .normal)
     }
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
         isTimerRunning = true
-        pauseButton.isEnabled = true
+        enableButton(theButton: pauseButton)
+        disableButton(theButton: startButton)
+        
+        // Check to see if User allows local notifications
+        let center = UNUserNotificationCenter.current()
+        
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus != .authorized {
+                // Notifications not allowed
+                return;
+                
+            }
+        }
+        
+        // Define the Notification Content
+        let content = UNMutableNotificationContent()
+        content.title = "Time's Up"
+        content.body = "It's time to Vote!"
+        content.sound = UNNotificationSound.default()
+        
+        
+        // Determine trigger time based on current time and fireDate in seconds
+        let firedInt = NSInteger(timer.fireDate.timeIntervalSinceNow)
+        let test1 = TimeInterval(currentRoundTimer) - timer.fireDate.timeIntervalSinceNow
+        
+        // Define the trigger for the notification
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(test1), repeats: false)
+        
+        
+        
+        // Schedule the Notification
+        let request = UNNotificationRequest(identifier: "TimesUp_Notification", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
     }
     
     func updateTimer() {
@@ -125,11 +208,23 @@ class ViewController: UIViewController {
             timer.invalidate()
             // Send alert to indicate "time's up!"
             // create a sound ID, in this case its the tweet sound.
-            let systemSoundID: SystemSoundID = 1016 //tweet
-            //let systemSoundID: SystemSoundID = 1304 //alarm
+           // let systemSoundID: SystemSoundID = 1016 //tweet
+            let systemSoundID: SystemSoundID = 1304 //alarm
+            disableButton(theButton: pauseButton)
+            disableButton(theButton: resetButton)
+
             
             // to play sound
             AudioServicesPlaySystemSound (systemSoundID)
+            
+            // Display Alert Dialog
+            let alertController = UIAlertController(title: "Time's Up", message:
+                "It's time to vote!", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+
+            self.present(alertController, animated: true, completion: nil)
+            
+            
         } else {
             seconds -= 1     //This will decrement(count down)the seconds.
             // myTimerLabel.text = "\(seconds)" //This will update the label.
@@ -148,8 +243,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.darkGray
-        pauseButton.isEnabled = false
+        disableButton(theButton: pauseButton)
+        
+        
     }
 
 
